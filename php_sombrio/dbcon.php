6260a52247feb70ec4443a68b16818f3f2a9cd32
DenClearSombrio <denclear12@gmail.com>
2025-04-11 12:43:04 +0800

    Add files via upload

---
+++ php_sombrio/dbcon.php
@@ -0,0 +1,18 @@
+<?php
+
+   define("HOSTNAME", "localhost");
+   define("USERNAME", "root");
+   define("PASSWORD", "");
+   define("DATABASE", "SOMBRIO_2B");
+
+   $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+   if(!$connection){
+      die("Connection failed");
+   }
+   else{
+    echo "yesss";
+   }
+
+
+
+   ?>
